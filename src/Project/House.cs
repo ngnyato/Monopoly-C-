f7cb70e8c3a93a7b26a653fd050c447121732109
Afonso Torres <afonsotorres953@gmail.com>
2025-12-15 22:40:59 +0000

    forgot to include House.cs

---
+++ src/Project/House.cs
@@ -0,0 +1,28 @@
+public class House 
+{   
+     // FUNNY if i put a doctor here in this class it becomes DOCTOR HOUSE ?!?!
+     // https://i.redd.it/0iqk8cd2yh9e1.jpeg
+
+    public string houseName { get; set;}
+    public string houseType { get; set;}  //Property, Train, Tax, Chance, Community, Special
+    public Player houseOwner { get; set;} 
+    public int housePrice { get; set;}
+    public int houseRent { get; set;}
+
+    public int x { get; set;}
+    public int y { get; set;}
+
+    public House( string houseName, string houseType, Player houseOwner, int housePrice, int houseRent, int x, int y) 
+    {
+        this.houseName = houseName;
+        this.houseType = houseType;
+        this.houseOwner = houseOwner;
+        this.housePrice = housePrice;
+        this.houseRent = houseRent;
+        this.x = x;
+        this.y = y;
+    }
+
+
+
+}
